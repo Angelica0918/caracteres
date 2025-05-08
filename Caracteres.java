2c1cdb76d5be9d0d58e18c6262c3515dba72c70e
Angelica0918 <20243ds136@utez.edu.mx>
2025-05-08 08:43:02 -0600

    iniciamos a imprimir

---
+++ Caracteres.java
@@ -0,0 +1,12 @@
+public class Caracteres{
+    public static void main(String[]args){
+        char letra = 'a';
+        char simbolo = 'S';
+        char numeroChar = '5';
+
+        System.out.println("letra=" + letra);
+        System.out.println("simbolo=" + simbolo);
+        System.out.println("numeroChar=" + numeroChar);
+        //el mas significa unir es como el %s %f
+    }
+}
